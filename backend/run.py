70ae84b8621c530250ba46cdd91ccd6b0feb4cd0
Samik Wangneo <wangneosamik@gmail.com>
2026-04-14 02:52:34 -0400

    port binding

--- backend/run.py
+++ backend/run.py
@@ -2,8 +2,10 @@
 Launch script for Aether backend.
 """
 
+import os
 import uvicorn
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info")
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, log_level="info")
